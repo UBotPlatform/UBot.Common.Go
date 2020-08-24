0d5fd887a0f57542d1ef953458014b1207de2fb5
qiqiworld <1354092549@qq.com>
2020-08-24 18:41:27 +0800

    fix: error occurred when non-subscribed event emitted

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/UBotPlatform/UBot.Common.Go
 go 1.14
 
 require (
-	github.com/1354092549/wsrpc v0.2.3
+	github.com/1354092549/wsrpc v0.3.0
 	github.com/gorilla/websocket v1.4.2
 )
--- helper.go
+++ helper.go
@@ -1,7 +1,13 @@
 package ubot
 
+import "reflect"
+
 func withDefault(value interface{}, defaultValue interface{}) interface{} {
 	if value == nil {
+		return defaultValue
+	}
+	rValue := reflect.ValueOf(value)
+	if !rValue.IsValid() || rValue.IsZero() {
 		return defaultValue
 	}
 	return value
