e66e6d90d589b5bd300a8eab7fa761bc704f6714
ArcticLampyrid <ArcticLampyrid@outlook.com>
2021-06-09 12:02:23 +0800

    feature: add helper for reading args

--- msgentity.go
+++ msgentity.go
@@ -13,3 +13,22 @@ var MsgTypePattern = regexp.MustCompile(`^[a-z0-9_\.]+$`)
 func IsValidMsgType(msgType string) bool {
 	return MsgTypePattern.MatchString(msgType)
 }
+
+func (e *MsgEntity) ArgOr(i int, defaultValue string) string {
+	if i < 0 || i >= len(e.Args) {
+		return defaultValue
+	}
+	return e.Args[i]
+}
+
+func (e *MsgEntity) NamedArgOr(name string, defaultValue string) string {
+	v, ok := e.NamedArgs[name]
+	if !ok {
+		return defaultValue
+	}
+	return v
+}
+
+func (e *MsgEntity) FirstArgOrEmpty() string {
+	return e.ArgOr(0, "")
+}
